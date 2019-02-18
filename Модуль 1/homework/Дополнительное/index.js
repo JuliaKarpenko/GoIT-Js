a0841e65c8cf65e276d48576de53d44b5fdb7819
Admin <jule4ka.jd@gmail.com>
2019-02-18 20:02:35 +0200

    first commit

---
+++ Модуль 1/homework/Дополнительное/index.js
@@ -0,0 +1,50 @@
+
+/* Создайте скрипт турагенства, продающего поездки в 3-х группах: sharm, hurgada и taba.
+  Кол-во мест в группах ограничено (создайте переменные для хранения мест в группах): 
+    * sharm - 15
+    * hurgada - 25
+    * taba - 6.
+  Когда пользователь посещает страницу, ему необходимо предложить ввести число необходимых мест,
+  результат сохранить в переменную.
+  Необходимо проверить являются ли введенные данные целым положительным числом. 
+  
+    - В случае неверного ввода от пользователя, скрипт показывает alert с текстом 
+      "Ошибка ввода" и больше ничего не делает.
+    - Если пользователь нажал Cancel, скрипт показывает alert с текстом "Нам очень жаль, приходите еще!".
+    - В случае верного ввода, последовательно проверить кол-во мест в группах, 
+      и кол-во необходимых мест введенных пользователем.
+  Если была найдена группа в которой количество мест больше либо равно необходимому, 
+  вывести сообщение через confirm, что есть место в группе такой-то, согласен ли 
+  пользоваетель быть в этой группе?
+    * Если ответ да, показать alert с текстом 'Приятного путешествия в группе <имя группы>'
+    * Если ответ нет, показать alert с текстом 'Нам очень жаль, приходите еще!'
+  
+  Если мест нигде нет, показать alert с сообщением 'Извините, столько мест нет ни в одной группе!'
+*/
+
+let sharm = 15;
+let hurgada = 25;
+let taba = 6;
+const error = 'Error';
+const canselMessage = 'We are so sorry, come again!'
+
+let userEnter = prompt('Enter a number');
+
+let isInteger = Number.isInteger(Number.parseInt(userEnter));
+
+
+if (isInteger === true && userEnter > 0 ) {
+  console.log("hi");
+  if (userEnter <= sharm && userEnter < hurgada) {
+    confirm('there is a place in the group sharm. Do you want to choose this gooup?');
+  } else if (userEnter <= hurgada) {
+    confirm('there is a place in the group hurgada. Do you want to choose this gooup?');
+  } else if (userEnter <= taba && userEnter < sharm) {
+    confirm('there is a place in the group taba. Do you want to choose this gooup?');
+  }
+} else if (userEnter == null) {
+  console.log("null");
+  alert(canselMessage);
+} else {
+  alert(error);
+}
---
+++ Модуль 1/практика/index.js
@@ -0,0 +1,221 @@
+"use strict";
+
+// var year = 2018;
+// let month = "November";
+// const date = 27;
+// let apple = 10;
+// let bananas = 5;
+// let lime = 100;
+
+// let fontSize = "5.5px";
+
+// // let summFruits = apple * bananas + lime;
+// // console.log('summFruits', summFruits);
+// // alert(`We are have ${summFruits} fruits`);
+// // const resultConfirm = confirm(`Is it ${month}?`);
+// // const resultPrompt = prompt('How old are you?');
+// // console.log('resultConfirm', resultConfirm);
+// // console.log('resultPrompt', resultPrompt);
+
+// console.log(typeof apple);
+// console.log(`тип яблоко и тип лайм = ${typeof apple === typeof lime}`);
+// console.log(`тип яблоко и тип месяц = ${typeof apple === typeof month}`);
+
+// console.log(`Index letter "N" in word "November" is ${month.indexOf("N")}`);
+// console.log(`Index letter "w" in word "November" is ${month.indexOf("w")}`);
+// console.log(`Index letter "o" in word "November" is ${month.indexOf("o")}`);
+
+// console.log(`Includes letter "o" in word "November" is ${month.includes("o")}`);
+// console.log(`Includes letter "w" in word "November" is ${month.includes("w")}`);
+
+// console.log(`Parsing Int of string "fontSize" is ${Number.parseInt(fontSize)}`);
+// console.log(
+//   `Parsing Float of string  "fontSize" is  ${Number.parseFloat(fontSize)}`
+// );
+
+// console.log(`Is "apple" is not a number? - ${isNaN(apple)}`);
+// console.log(`Is "month" is not a number? - ${isNaN(month)}`);
+
+// const a = 0.3;
+// const b = 0.5;
+
+// console.log("Summa a + b = ", a + b);
+// console.log("Summa with to.Fixed(10) is a + b = ", (a + b).toFixed(10));
+
+// const text = "ПреобраЗование В цеЛое Число";
+
+// console.log("Преобразование к нижнему регистру = ", text.toLowerCase());
+// console.log("Преобразование к верхнему регистру = ", text.toLocaleUpperCase());
+
+// const name = "John";
+// const surname = "Cannor";
+// const years = 40;
+// const height = 180;
+
+// console.log(`Hello, my name is ${name} ${surname}. I'm ${years} and ${height}`);
+// console.log(`Hello, my name is ${name} ${surname}. I'm ${years} and ${height + 2 + 'sm'}`);
+
+// ======================================part 2======================================================
+
+// const num = 20;
+// const result = num < 30 && num > 10;
+// console.log(result);
+// const text = 'texttext';
+// console.log(text.length < 30 && text.length > 5);
+// console.log(text.length < 30 && text.length > 9);
+
+// console.log(text.length < 30 || text.length > 9);
+
+// // =============================================
+
+// let hours = 7;
+// let minutes = 3;
+// let seconds = 43;
+// if (hours <= 10) {
+//   hours = '0' + hours;
+// }else{
+
+// }
+// if (minutes <= 10) {
+//   minutes = '0' + minutes;
+// }else{
+  
+// }
+// if (seconds <= 10) {
+//   seconds = '0' + seconds;
+// }else{
+  
+// }
+
+// const time = `${hours}:${minutes}:${seconds}`;
+
+// console.log('Time is: ', time);
+
+
+// ===================================
+
+// const monthName = 'January';
+// let result;
+
+// if (monthName === "June") {
+//   result = 'summer';
+// } else {
+//   result = 'winter';
+// }
+
+// console.log(result);
+
+// let season = monthName === 'June' ? 'summer' : 'winter';
+
+// console.log(season);
+
+// ================================
+
+// const monthName = "January";
+
+// const season = monthName === "June"
+//   ? "summer" : monthName === "May"
+//   ? "spring" : monthName === "September"
+//   ? "autumn" : "winter";
+
+// console.log(season);
+
+// ========================================
+
+// const monthName = "January";
+// let season;
+
+// switch (monthName) {
+//   case "June":
+//     season = "summer";
+//     break;
+//   case "May":
+//     season = "spring";
+//     break;
+//   case "September":
+//     season = "autumn";
+//     break;
+//   default:
+//     season = "winter";
+// }
+
+// console.log(season); //winter
+
+
+// ========================task 2=========
+
+// const day = 2;
+// const month = 10;
+// const year = 2017;
+
+// const date = day + '\\' + month + '\\' + year; 
+
+// console.log(date); // 02\10\2017
+
+// ==========================task 9==============
+
+/* 
+  Время состоит из: 
+    часов(hours)
+    минут (minutes)
+    секунд(seconds).
+  
+  Время должно всегда выводиться в формате xx:xx:xx
+  Например: 01:12:04 или 14:03:45
+  
+  Составляющие времени не гарантированно состоят из 2-х цифр!
+  
+  Напишите скрипт который проверяет каждую составляющую,
+  тоесть значения переменных hours, minutes, seconds 
+  и добавлят впереди 0 если необходимо.
+*/
+
+let hours = 7;
+let minutes = 3;
+let seconds = 42;
+
+hours = String(hours);
+minutes = String(minutes);
+seconds = String(seconds);
+
+if (hours.length === 1) {
+  hours = '0' + hours;
+}
+
+if (minutes.length === 1) {
+  minutes = '0' + minutes;
+}
+
+if (seconds.length === 1) {
+  seconds = '0' + seconds;
+}
+
+const time = `${hours}:${minutes}:${seconds}`;
+console.log('Time is: ', time);
+
+// ================ with function ===================
+
+// let hours = 7;
+// let minutes = 3;
+// let seconds = 42;
+
+// hours = String(hours);
+// minutes = String(minutes);
+// minutes = String(seconds);
+
+// function addZerro (val) {
+//   if (val.length === 1) {
+//     return val = '0' + val;
+//   }
+// }
+
+// addZerro(hours);
+// console.log(hours);
+// addZerro(minutes);
+// addZerro(seconds);
+
+
+// const time = `${hours}:${minutes}:${seconds}`;
+// console.log('Time is: ', time);
+
+
